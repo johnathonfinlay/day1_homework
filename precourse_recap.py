86d9816a820c5072f1174e75893263f32a9c0df9
Johnathon Finlay <jfinlay@gmail.com>
2023-01-09 16:02:19 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,17 @@
+number1 = 12
+number2 = 13
+number3 = 25
+org_name = "CodeClan"
+greek_py = 3.14
+my_name = "Johnathon Finlay"
+print("My name is ")
+print(my_name)
+print("The Greeks reckon that Pi is ")
+print(greek_py)
+print(number1*number2)
+print(number3-number2)
+print(number2/number1)
+
+
+
+
